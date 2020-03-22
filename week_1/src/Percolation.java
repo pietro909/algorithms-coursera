4f3ff2421c0c05d8c655adfd9c011ac45e0a883f
Pietro Grandi <pietro.grandi.3d@gmail.com>
2020-03-22 19:49:00 +0100

    72/100
    Not passed. I'm tired now ;-(

--- week_1/src/Percolation.java
+++ week_1/src/Percolation.java
@@ -7,18 +7,19 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 By convention, the row and column indices are integers between 1 and n, where (1, 1) is the upper-left site
  */
 public class Percolation {
-    private WeightedQuickUnionUF wquUF;
-    private int[][] grid;
+    private final WeightedQuickUnionUF wquUF;
+    private final boolean[][] grid;
+    private int openSites = 0;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int size) {
         if (size <= 0) {
             throw new IllegalArgumentException("Grid size must be > 0");
         }
-        grid = new int[size][size];
+        grid = new boolean[size][size];
         for (var i = 0; i < size; i++) {
             for (var j = 0; j < size; j++) {
-                grid[i][j] = 0;
+                grid[i][j] = false;
             }
         }
         wquUF = new WeightedQuickUnionUF(size * size);
@@ -40,8 +41,15 @@ public class Percolation {
     public void open(int row, int col) {
         assertBoundaries(row);
         assertBoundaries(col);
+
+        // already open
+        if (grid[row - 1][col - 1]) {
+            return;
+        }
+
         int size = grid.length;
-        grid[row - 1][col - 1] = 1;
+        grid[row - 1][col - 1] = true;
+        openSites += 1;
 
         // west
         if (col > 1 && isOpen(row, col - 1)) wquUF.union(toIndex(row, col), toIndex(row, col - 1));
@@ -60,39 +68,49 @@ public class Percolation {
     public boolean isOpen(int row, int col) {
         assertBoundaries(row);
         assertBoundaries(col);
-        return grid[row - 1][col - 1] == 1;
+        return grid[row - 1][col - 1];
     }
 
     // is the site (row, col) full?
+    /*
+    A full site refers to an open site that is connected to another open site in the top row.
+    You can check if a site is full by using a virtual site that is connected to every site in the top row.
+     */
     public boolean isFull(int row, int col) {
         assertBoundaries(row);
         assertBoundaries(col);
-        return grid[row - 1][col - 1] == 0;
+
+        if (!grid[row - 1][col - 1]) {
+            return false;
+        }
+
+        int site = toIndex(row, col);
+        for (var c = 1; c <= grid.length; c += 1) {
+            int rootTop = toIndex(1, c);
+            if (wquUF.connected(rootTop, site)) return true;
+        }
+        return false;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        int open = 0;
-        int size = grid.length;
-        for (var i = 0; i < size; i++) {
-            for (var j = 0; j < size; j++) {
-                if (grid[i][j] == 1) {
-                    open++;
-                }
-            }
-        }
-        return open;
+        return openSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
         // the last row has at least one root in common with first row?
         int size = grid.length;
+        if (size == 1) {
+            return grid[0][0];
+        }
         for (var row = 1; row <= size; row++) {
             for (var column = 1; column <= size; column++) {
+                boolean siteTop = grid[1][column - 1];
+                boolean siteBottom = grid[size - 1][row - 1];
                 int rootTop = toIndex(1, column);
                 int rootBottom = toIndex(size, row);
-                if (wquUF.connected(rootTop, rootBottom)) {
+                if (siteTop && siteBottom && wquUF.connected(rootTop, rootBottom)) {
                     return true;
                 }
             }
@@ -100,15 +118,16 @@ public class Percolation {
         return false;
     }
 
-    public String print() {
+    private String print() {
         int size = grid.length;
-        StringBuilder output = new StringBuilder("---\n");
+        StringBuilder output = new StringBuilder("--------------------------\n");
         for (var i = 0; i < size; i++) {
             for (var j = 0; j < size; j++) {
                 output.append(grid[i][j]).append(" ");
             }
             output.append("\n");
         }
+        output.append("--------------------------");
         return output.toString();
     }
 
@@ -119,15 +138,23 @@ public class Percolation {
         Percolation percolation = new Percolation(size);
 
         percolation.open(2, 3);
-        StdOut.println(percolation.print());
         StdOut.println("is 1,1 open? " + percolation.isOpen(1, 1));
         StdOut.println("is 2,3 open? " + percolation.isOpen(2, 3));
+        StdOut.println("is 1,1 full? " + percolation.isFull(1, 1));
+        StdOut.println("is 2,3 full? " + percolation.isFull(2, 3));
+        percolation.open(2, 3);
+        percolation.open(2, 3);
+        percolation.open(2, 3);
 
         StdOut.println(percolation.percolates() + " = false");
+        StdOut.println("numberOfOpenSites: " + percolation.numberOfOpenSites());
+
         percolation.open(1, 3);
         percolation.open(3, 3);
-        StdOut.println(percolation.print());
-        StdOut.println("total: " + percolation.numberOfOpenSites());
+        StdOut.println("is 2,3 full? " + percolation.isFull(2, 3));
+        StdOut.println("numberOfOpenSites: " + percolation.numberOfOpenSites());
         StdOut.println(percolation.percolates() + " = true");
+        StdOut.println(percolation.print());
+
     }
 }
--- week_1/src/PercolationStats.java
+++ week_1/src/PercolationStats.java
@@ -4,44 +4,56 @@ import edu.princeton.cs.algs4.StdStats;
 
 
 public class PercolationStats {
+    private static final double CONFIDENCE_95 = 1.96;
+    private final double[] thresholds;
+    private final int times;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        Percolation percolation = new Percolation(n);
-
-        while(!percolation.percolates()) {
-            int row = StdRandom.uniform(1, n+1);
-            int col = StdRandom.uniform(1, n+1);
-            percolation.open(row, col);
+        if (n < 1 || trials < 1) {
+            throw new IllegalArgumentException("both n and trials must be > 0");
         }
+        thresholds = new double[trials];
+        times = trials;
 
-        StdOut.println(percolation.print());
-        StdOut.println("percolated with " + percolation.numberOfOpenSites());
+        for (var t = 0; t < trials; t++) {
+            Percolation percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                percolation.open(row, col);
+            }
+            double threshold = ((double) percolation.numberOfOpenSites()) / (((double) n) * ((double) n));
+            thresholds[t] = threshold;
+        }
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return 0.42;
+        return StdStats.mean(thresholds);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 0.42;
+        return StdStats.stddev(thresholds);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return 0.42;
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(times * times));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return 0.42;
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(times * times));
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        PercolationStats stats = new PercolationStats(6, 2);
+        PercolationStats stats = new PercolationStats(20, 10);
 
+        StdOut.println("mean\t\t= " + stats.mean());
+        StdOut.println("stddev\t\t= " + stats.stddev());
+        StdOut.println("95% confidence interval\t= [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
     }
 }
