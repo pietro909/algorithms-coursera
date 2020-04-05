4b2aa6a242e21ee8aa08ed1f53993450d9f54bfd
Pietro Grandi <pietro.grandi.3d@gmail.com>
2020-04-05 19:54:12 +0200

    a huge mess, clearly is wrong

--- week-3/src/BruteCollinearPoints.java
+++ week-3/src/BruteCollinearPoints.java
@@ -1,40 +1,50 @@
-public class BruteCollinearPoints {
-    Point[] points;
+import edu.princeton.cs.algs4.StdOut;
 
-    public BruteCollinearPoints(Point[] _points) {
+public class BruteCollinearPoints {
+    private LineSegment[] results;
+
+    public BruteCollinearPoints(Point[] points) {
         //Throw an IllegalArgumentException
         //      if the argument to the constructor is null,
         //      if any point in the array is null, or
         //      if the argument to the constructor contains a repeated point.
-        this.points = _points;
+
         if (points == null) throw new IllegalArgumentException("points is null");
-        for (var i = 0; i < points.length; i++)
-            if (points[i] == null) throw new IllegalArgumentException("point is null at " + i);
+
+        results = findSegments(points);
     }
 
-    public int numberOfSegments() {
-        return segments().length;
-    }
+    private LineSegment[] findSegments(Point[] points) {
+        // The method segments() should include each line segment containing 4 points exactly once.
+        // If 4 points appear on a line segment in the order p→q→r→s,
+        // then you should include either the line segment p→s or s→p (but not both) and you
+        // should not include subsegments such as p→r or q→r.
+        //
+        // For simplicity, we will not supply any input to BruteCollinearPoints that has 5 or more collinear points.
 
-    // The method segments() should include each line segment containing 4 points exactly once.
-    // If 4 points appear on a line segment in the order p→q→r→s,
-    // then you should include either the line segment p→s or s→p (but not both) and you
-    // should not include subsegments such as p→r or q→r.
-    //
-    // For simplicity, we will not supply any input to BruteCollinearPoints that has 5 or more collinear points.
-
-    // growth: n4 in the worst case
-    public LineSegment[] segments() {
-        LineSegment[] results = new LineSegment[points.length/4];
+        // growth: n4 in the worst case
+        LineSegment[] results = new LineSegment[points.length / 4];
         int counter = 0;
         // To check whether the 4 points p, q, r, and s are collinear, check whether the three
         // slopes between p and q, between p and r, and between p and s are all equal.
-        for (var p = 0; p < points.length - 4; p++) {
-            for (var q = p + 1; q < points.length - 3; q++) {
-                for (var r = q + 1; r < points.length - 2; r++) {
+        for (var p = 0; p < points.length - 1; p++) {
+            for (var q = p + 1; q < points.length - 1; q++) {
+                for (var r = q + 1; r < points.length - 1; r++) {
                     for (var s = r + 1; s < points.length - 1; s++) {
-                        if (points[p].slopeTo(points[q]) == points[p].slopeTo(points[r]) &&
-                        points[p].slopeTo(points[q]) == points[p].slopeTo(points[s])) {
+//                        StdOut.println("... " + p + ", " + q + ", " + r + ", " + s);
+
+                        double slopePToQ = points[p].slopeTo(points[q]);
+                        double slopePToR = points[p].slopeTo(points[r]);
+                        double slopePToS = points[p].slopeTo(points[s]);
+
+                        if (slopePToQ == Double.NEGATIVE_INFINITY ||
+                                slopePToR == Double.NEGATIVE_INFINITY ||
+                                slopePToS == Double.NEGATIVE_INFINITY
+                        ) {
+                            throw new IllegalArgumentException("same point at " + p + ", " + q + ", " + r + ", " + s);
+                        }
+//                        StdOut.println("checking "+slopePToQ+" == "+slopePToR+" == "+slopePToS);
+                        if (slopePToQ == slopePToR && slopePToQ == slopePToS) {
                             results[counter] = new LineSegment(points[p], points[s]);
                             counter++;
                         }
@@ -42,6 +52,14 @@ public class BruteCollinearPoints {
                 }
             }
         }
+        return results;
+    }
+
+    public int numberOfSegments() {
+        return segments().length;
+    }
+
+    public LineSegment[] segments() {
         return results;
     }
 }
--- week-3/src/FastCollinearPoints.java
+++ week-3/src/FastCollinearPoints.java
@@ -1,16 +1,130 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class FastCollinearPoints {
+    private LineSegment[] results;
+    private int counter = 0;
+
     public FastCollinearPoints(Point[] points) {
-        // finds all line segments containing 4 or more points
+        if (points == null) throw new IllegalArgumentException("points is null");
+        LineSegment[] allSegments = findSegments(points);
+        results = new LineSegment[counter];
+        var j = 0;
+//        StdOut.println("all segments  " + allSegments.length);
+
+        for (var i = 0; i < allSegments.length; i++) {
+//            StdOut.println("check " + allSegments[i]);
+
+            if (allSegments[i] != null) {
+                results[j] = allSegments[i];
+                j++;
+            }
+        }
     }
 
-    // the number of line segments
-    public           int numberOfSegments()   {
-        return 0;
+    private LineSegment[] findSegments(Point[] points) {
+        LineSegment[] atMostResults = new LineSegment[points.length];
+        int size = points.length - 1;
+        Point[] sortedPoints = new Point[size];
+
+        double[] slopes = new double[points.length];
+
+//        Double previousSlope = Double.NEGATIVE_INFINITY;
+
+        for (var p = 0; p < size - 1; p++) {
+            Point pointP = points[p];
+
+            if (pointP == null) {
+                throw new IllegalArgumentException("point at is null at " + p);
+            }
+
+            // mutation ;-(
+//            StdOut.println("copy " + points.length + " to " + sortedPoints.length + " except " + p);
+            copyOtherPoints(points, sortedPoints, p);
+//            copyOtherPoints(points, sortedPoints, p);
+            // sort by slope of P
+
+            Arrays.sort(sortedPoints, pointP.slopeOrder());
+//            Arrays.sort(sortedPoints);
+
+            printArray(sortedPoints);
+
+            // TODO: check for duplicates
+
+            for (var q = 0; q < size - 1; q++) {
+                Point pointQ = sortedPoints[0];
+                double currentSlope = pointP.slopeTo(pointQ);
+
+//                StdOut.println("... " + previousSlope + " vs " + currentSlope);
+
+//                if (currentSlope == previousSlope) {
+//                    StdOut.println("skipping " + currentSlope);
+//                    continue;
+//                }
+
+                StdOut.println("proceeding with " + pointP + ", " + pointQ + " = " + currentSlope);
+//                previousSlope = currentSlope;
+
+                Point startPoint = pointP;
+                // candidate as end point
+                Point endPoint = sortedPoints[q++];
+                // actual end point
+                Point previousPoint = endPoint;
+                while (endPoint != null &&
+                        currentSlope == pointP.slopeTo(endPoint)
+                ) {
+                    StdOut.println(" - compare " + startPoint + " to " + endPoint + " = " + startPoint.compareTo(endPoint));
+                    if (startPoint.compareTo(endPoint) > 0) {
+                        // invert points
+                        previousPoint = startPoint;
+                        startPoint = endPoint;
+                        endPoint = sortedPoints[q++];
+                    } else {
+                        previousPoint = endPoint;
+                        endPoint = sortedPoints[q++];
+                    }
+                }
+
+                StdOut.println(" slope: " + startPoint.slopeTo(previousPoint) + " == " + startPoint.slopeTo(pointQ));
+
+
+                if (previousPoint != null) {
+                    atMostResults[counter] = new LineSegment(pointP, previousPoint);
+                    counter++;
+                    break;
+                }
+            }
+        }
+        return atMostResults;
     }
 
-    // the line segments
-    public LineSegment[] segments()     {
-        // the line segments
-        return new LineSegment[0];
+    private void printArray(Point[] a) {
+        StringBuilder sb = new StringBuilder("[ ");
+        for (var i = 0; i < a.length; i++) {
+            sb.append(a[i].toString() + ", ");
+        }
+        sb.append("]");
+        StdOut.println(sb);
+    }
+
+    private void copyOtherPoints(Point[] from, Point[] to, int except) {
+        var j = 0;
+        for (var i = 0; i < from.length; i++) {
+            if (i != except) {
+                to[j] = from[i];
+                j++;
+            }
+        }
+    }
+
+    public int numberOfSegments() {
+        return counter;
+    }
+
+    public LineSegment[] segments() {
+        // TODO: null
+        return results;
     }
 }
