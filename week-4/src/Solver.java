c23d17b9c810aa994f73ec8f6491bb5ffcf6fd59
Pietro Grandi <pietro.grandi.3d@gmail.com>
2020-04-15 17:38:41 +0200

    next: optimization

--- week-4/src/Solver.java
+++ week-4/src/Solver.java
@@ -1,9 +1,19 @@
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.function.Consumer;
 
 public class Solver {
-    private MinPQ<SearchNode> queue = new MinPQ<SearchNode>();
+    private final LinkedList<Board> solution = new LinkedList<Board>();
+    private MinPQ<SearchNode> queue = new MinPQ<SearchNode>(1, new Comparator<SearchNode>() {
+        @Override
+        public int compare(SearchNode o1, SearchNode o2) {
+            return (o1.moves + o1.board.manhattan()) - (o2.moves + o2.board.manhattan());
+        }
+    });
 
     //the initial board, 0 moves, and a null previous search node
     private class SearchNode {
@@ -23,8 +33,40 @@ public class Solver {
         if (initial == null)
             throw new IllegalArgumentException();
 
-        SearchNode node = new SearchNode(initial, initial.manhattan(), null);
+//        StdOut.println("init solver");
+
+        int moves = 0;
+        SearchNode node = new SearchNode(initial, moves, null);
         queue.insert(node);
+
+        while (!node.board.isGoal()) {
+            node = queue.delMin();
+            Iterator<Board> neighbors = node.board.neighbors().iterator();
+//            StdOut.println(" --> node " + node.board);
+            if (node.board.isGoal()) {
+//                StdOut.println(" --> GOAL");
+                break;
+            }
+
+            moves++;
+
+//            StdOut.println(" --> move " + moves);
+
+            while (neighbors.hasNext()) {
+                Board neighbor = neighbors.next();
+//                StdOut.println(" --> queue " + neighbor);
+                queue.insert(new SearchNode(neighbor, moves, node));
+            }
+        }
+
+//        StdOut.println("done solver");
+
+        while (node != null) {
+//            if (!node.board.isGoal()) {
+                solution.addFirst(node.board);
+//            }
+            node = node.previous;
+        }
     }
 
     // is the initial board solvable? (see below)
@@ -34,17 +76,38 @@ public class Solver {
 
     // min number of moves to solve initial board
     public int moves() {
-        return 0;
+        return solution.size()-1;
     }
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-        return new LinkedList<Board>();
+        return solution;
     }
 
     // test client (see below)
     public static void main(String[] args) {
+        int[][] goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+        int[][] three1 = {{0, 1, 3}, {4, 2, 5}, {7, 8, 6}};
 
+        Solver s1 = new Solver(new Board(goal));
+        StdOut.println("== goal ==");
+        StdOut.println(" - [0] moves:\t" + s1.moves());
+        s1.solution().forEach(new Consumer<Board>() {
+            @Override
+            public void accept(Board board) {
+                StdOut.println(board);
+            }
+        });
+
+        Solver s2 = new Solver(new Board(three1));
+        StdOut.println("== goal ==");
+        StdOut.println(" - [4] moves:\t" + s2.moves());
+        s2.solution().forEach(new Consumer<Board>() {
+            @Override
+            public void accept(Board board) {
+                StdOut.println(board);
+            }
+        });
     }
 
 }
