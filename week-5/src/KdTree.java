ecdb56bda180dc4005c4828a05605dee807fb2f9
Pietro Grandi <pietro.grandi.3d@gmail.com>
2020-05-01 23:37:48 +0200

    better draw

--- week-5/src/KdTree.java
+++ week-5/src/KdTree.java
@@ -4,63 +4,36 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 
-import java.awt.Color;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.TreeSet;
-
-/*
-// smaller is E, larger is S (from the book)
-    private Node rotateLeft(Node smaller) {
-        Node larger = smaller.right;
-        smaller.right = larger.left;
-        larger.left = smaller;
-        larger.color = smaller.color;
-        smaller.color = RED;
-        larger.size = smaller.size;
-        smaller.size = 1 + size(smaller.left) + size(smaller.right);
-        return larger;
-    }
-
-    private Node rotateRight(Node larger) {
-        Node smaller = larger.right;
-        larger.left = smaller.right;
-        smaller.right = larger;
-        smaller.color = larger.color;
-        larger.color = RED;
-        smaller.size = larger.size;
-        larger.size = 1 + size(larger.left) + size(larger.right);
-        return smaller;
-    }
- */
+import static java.lang.Thread.sleep;
 
 public class KdTree {
     private Node root = null;
 
-    private static final boolean VERTICAL = true;
-    private static final boolean HORIZONTAL = false;
-    private static final boolean RED = true;
-    private static final boolean BLACK = false;
-
-    private boolean isRed(Node x) {
-        if (x == null) return false;
-        return x.color == RED;
-    }
+    private static final boolean LEFT = true;
+    private static final boolean RIGHT = false;
 
     private class Node {
-        Point2D value;
-        int size = 0;
-        boolean color;
-        boolean alignment;
-        Node left, right;
-        Node parent;
-        boolean isLower;
+        Point2D point;
+        int size = 1;
+        Node left = null, right = null;
 
-        Node(Point2D p, boolean v) {
-            value = p;
-            alignment = v;
+        Node(Point2D point) {
+            this.point = point;
         }
+    }
+
+    private boolean isVertical(int level) {
+        return level % 2 > 0;
+    }
+
+    public boolean isEmpty()                      // is the set empty?
+    {
+        return size() == 0;
+    }
+
+    public int size()                         // number of points in the set
+    {
+        return size(root);
     }
 
     private int size(Node node) {
@@ -68,107 +41,102 @@ public class KdTree {
         return node.size;
     }
 
-    public void put(Point2D point) {
-        nonNull(point);
-        if (root == null) {
-            root = new Node(point, VERTICAL);
-            root.isLower = true; // hack
-        } else {
-            root = put(point, root);
-        }
-    }
-
-    private Node put(Point2D point, Node destination) {
-        boolean toLeft = destination.alignment == VERTICAL ? point.x() < destination.value.x() : point.y() < destination.value.y();
-        if (toLeft) {
-            // to the left
-            if (destination.left == null) {
-                Node node = new Node(point, !destination.alignment);
-                node.parent = destination;
-                node.isLower = true;
-                destination.left = node;
-            } else {
-                destination.left = put(point, destination.left);
-            }
-        } else {
-            // to the right
-            if (destination.right == null) {
-                Node node = new Node(point, !destination.alignment);
-                node.parent = destination;
-                node.isLower = false;
-                destination.right = node;
-
-            } else {
-                destination.right = put(point, destination.right);
-            }
-        }
-        return destination;
-    }
-
-    // construct an empty set of points
-    public void KdTree() {
-        // ???
-    }
-
-    public boolean isEmpty()                      // is the set empty?
-    {
-        return root == null;
-    }
-
-    public int size()                         // number of points in the set
-    {
-        return root.size;
-    }
-
     private void nonNull(Object a) {
         if (a == null)
             throw new IllegalArgumentException("can't be null");
     }
 
-    public void insert(Point2D p)              // add the point to the set (if it is not already in the set)
+    public void insert(Point2D point)              // add the point to the set (if it is not already in the set)
     {
-        nonNull(p);
-        put(p);
+        nonNull(point);
+        root = put(root, point, null, 1);
+    }
+
+    private Node put(Node destination, Point2D point, Node parent, int level) {
+        // empty tree
+        if (destination == null) {
+            return new Node(point);
+        }
+
+        if (destination.point.equals(point)) return destination;
+
+        if (isVertical(level)) {
+            // Key = x
+            // Val = y
+            int compare = Double.compare(point.x(), destination.point.x());
+            if (compare < 0) destination.left = put(destination.left, point, destination, level + 1);
+                // same x, got to the right tree
+            else destination.right = put(destination.right, point, destination, level + 1);
+        } else {
+            // Key = y
+            // Val = x
+            int compare = Double.compare(point.y(), destination.point.y());
+            if (compare < 0) destination.left = put(destination.left, point, destination, level + 1);
+            else destination.right = put(destination.right, point, destination, level + 1);
+        }
+
+        destination.size = 1 + size(destination.left) + size(destination.right);
+
+        return destination;
     }
 
     public boolean contains(Point2D p)            // does the set contain point p?
     {
         nonNull(p);
-        return false;
+        return contains(root, p, 1);
+    }
+
+    private boolean contains(Node node, Point2D point, int level)            // does the set contain point p?
+    {
+        nonNull(point);
+        if (node == null) return false;
+
+        if (node.point.compareTo(point) == 0) return true;
+        if (isVertical(level)) {
+            int compare = Double.compare(point.x(), node.point.x());
+            if (compare < 0) return contains(node.left, point, level + 1);
+            return contains(node.right, point, level + 1);
+        } else {
+            int compare = Double.compare(point.y(), node.point.y());
+            if (compare < 0) return contains(node.left, point, level + 1);
+            return contains(node.right, point, level + 1);
+        }
     }
 
     public void draw()                         // draw all points to standard draw
     {
-        draw(root);
+        draw(root, 1, new RectHV(0, 0, 1, 1));
     }
 
-    private void draw(Node node) {
+    private void draw(Node node, int level, RectHV box) {
         if (node == null) return;
-        double x = node.value.x();
-        double y = node.value.y();
+        double x = node.point.x();
+        double y = node.point.y();
 
-        StdDraw.setPenRadius(0.002);
-        if (node.alignment == VERTICAL) {
-            StdDraw.setPenColor(Color.RED);
-            double yLimit = node.parent == null ? 1 : node.parent.value.y();
-            if (node.isLower) {
-                StdDraw.line(x, 0, x, yLimit);
-            } else {
-                StdDraw.line(x, yLimit, x, 1);
-            }
-        } else {
-            StdDraw.setPenColor(Color.BLUE);
-            if (node.isLower) {
-                StdDraw.line(0, y, node.parent.value.x(), y);
-            } else {
-                StdDraw.line(node.parent.value.x(), y, 1, y);
-            }
-        }
+        StdDraw.setPenColor();
         StdDraw.setPenRadius(0.008);
         StdDraw.point(x, y);
+        StdDraw.text(x, y, "(" + x + " " + y + ")");
+//        StdDraw.setPenRadius(0.001);
+//        StdDraw.line(box.xmin(), box.ymin(), box.xmax(), box.ymin());
+//        StdDraw.line(box.xmax(), box.ymin(), box.xmax(), box.ymax());
+//        StdDraw.line(box.xmax(), box.ymax(), box.xmin(), box.ymax());
+//        StdDraw.line(box.xmin(), box.ymax(), box.xmin(), box.ymin());
 
-        draw(node.left);
-        draw(node.right);
+        StdDraw.setPenRadius(0.002);
+        if (isVertical(level)) {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(x, box.ymin(), x, box.ymax());
+            // this is  left/right
+            draw(node.left, level + 1, new RectHV(box.xmin(), box.ymin(), node.point.x(), box.ymax()));
+            draw(node.right, level + 1, new RectHV(node.point.x(), box.ymin(), box.xmax(), box.ymax()));
+        } else {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(box.xmin(), y, box.xmax(), y);
+            // this is bottom/top
+            draw(node.left, level + 1, new RectHV(box.xmin(), box.ymin(), box.xmax(), node.point.y()));
+            draw(node.right, level + 1, new RectHV(box.xmin(), node.point.y(), box.xmax(), box.ymax()));
+        }
     }
 
 
@@ -178,23 +146,26 @@ public class KdTree {
     {
         nonNull(rect);
         Queue<Point2D> queue = new Queue<Point2D>();
-        range(root, queue, rect);
+        range(root, queue, rect, 1);
         return queue;
     }
 
-    private void range(Node node, Queue<Point2D> queue, RectHV rect) {
+    private void range(Node node, Queue<Point2D> queue, RectHV rect, int level) {
         if (node == null) return;
-        if (rect.contains(node.value)) {
-            range(node.left, queue, rect);
-            queue.enqueue(node.value);
-            range(node.right, queue, rect);
+
+//        StdOut.println("--> " + node.point + " ? " + rect.contains(node.point));
+
+        if (rect.contains(node.point)) {
+            range(node.left, queue, rect, level + 1);
+            queue.enqueue(node.point);
+            range(node.right, queue, rect, level + 1);
         } else {
-            if (node.alignment == VERTICAL) {
-                if (Double.compare(rect.xmax(), node.value.x()) < 0) range(node.left, queue, rect);
-                else range(node.right, queue, rect);
+            if (isVertical(level)) {
+                if (Double.compare(rect.xmax(), node.point.x()) < 0) range(node.left, queue, rect, level + 1);
+                else range(node.right, queue, rect, level + 1);
             } else {
-                if (Double.compare(rect.ymax(), node.value.y()) < 0) range(node.right, queue, rect);
-                else range(node.right, queue, rect);
+                if (Double.compare(rect.ymax(), node.point.y()) < 0) range(node.right, queue, rect, level + 1);
+                else range(node.right, queue, rect, level + 1);
             }
         }
     }
@@ -202,26 +173,52 @@ public class KdTree {
     public Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
     {
         nonNull(p);
+        if (isEmpty()) return null;
+
         return null;
     }
 
-    public static void main(String[] args)                  // unit testing of the methods (optional)
+    public static void main(String[] args) throws InterruptedException                  // unit testing of the methods (optional)
     {
         StdDraw.enableDoubleBuffering();
         StdDraw.clear();
 
         KdTree pset1 = new KdTree();
-//        for (var i = 0; i < 10; i += 1) {
+//        for (var i = 0; i < 20; i += 1) {
 //            double x = StdRandom.uniform(0, 1.0);
 //            double y = StdRandom.uniform(0, 1.0);
 //            pset1.insert(new Point2D(x, y));
 //        }
-        pset1.insert(new Point2D(0.7, 0.2));
-        pset1.insert(new Point2D(0.5, 0.4));
-        pset1.insert(new Point2D(0.2, 0.3));
-        pset1.insert(new Point2D(0.4, 0.7));
-        pset1.insert(new Point2D(0.9, 0.6));
-        pset1.draw();
+//        pset1.insert(new Point2D(0.7, 0.2));
+//        pset1.insert(new Point2D(0.5, 0.4));
+//        pset1.insert(new Point2D(0.2, 0.3));
+//        pset1.insert(new Point2D(0.4, 0.7));
+//        pset1.insert(new Point2D(0.9, 0.6));
+//        pset1.insert(new Point2D(0.9, 0.6));
+
+        int t = 500;
+
+        Point2D[] points = {
+                new Point2D(0.7, 0.4),
+                new Point2D(0.3, 0.2),
+                new Point2D(0.8, 0.5),
+                new Point2D(0.52, 0.25),
+                new Point2D(0.80, 0.15),
+                new Point2D(0.6, 0.75),
+                new Point2D(0.45, 0.90),
+                new Point2D(0.75, 0.30),
+                new Point2D(0.25, 0.70),
+                new Point2D(0.50, 0.80),
+                new Point2D(0.38, 0.38)
+        };
+
+        for (int i = 0; i < points.length; i++) {
+            pset1.insert(points[i]);
+            pset1.draw();
+            StdDraw.show();
+            sleep(t);
+        }
+
 
 //        Point2D pset1Nearest1Query = new Point2D(0.5, 0.75);
 //        Point2D pset1Nearest1Result = pset1.nearest(pset1Nearest1Query);
@@ -234,11 +231,27 @@ public class KdTree {
 //        StdDraw.setPenColor(Color.ORANGE);
 //        StdDraw.point(pset1Nearest1Result.x(), pset1Nearest1Result.y());
 //        StdDraw.setPenColor();
+        StdOut.println("\tsize\t[5]\t" + pset1.size());
+
         StdOut.println("\trange\t[(0.0, 0.0 ), (1.0, 1.0)]");
         for (Point2D p : pset1.range(new RectHV(0, 0, 1, 1))) {
             StdOut.println("\t\t" + p);
         }
 
+//        RectHV range = new RectHV(0.42, 0.64, 0.86, 0.93);
+//        StdDraw.setPenColor(StdDraw.CYAN);
+//        StdDraw.setPenRadius(0.004);
+//        StdDraw.rectangle(
+//                range.xmin() + ((range.xmax() - range.xmin()) / 2),
+//                range.ymin() + ((range.ymax() - range.ymin()) / 2),
+//                range.width() / 2,
+//                range.height() / 2
+//        );
+//        StdDraw.setPenColor(StdDraw.ORANGE);
+//        for (Point2D p : pset1.range(range)) {
+//            StdOut.println("\t\t" + p);
+//            StdDraw.circle(p.x(), p.y(), 0.1);
+//        }
         StdDraw.show();
     }
 }
